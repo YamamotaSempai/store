c0230f4dfc970d2427be8dadeb5acc90f58efa0f
YamamotaSempai <aidos.jantleu@gmail.com>
2019-12-27 09:35:32 +0600

    fix

--- src/main/java/kz/aa/store/items/repository/ItemJdbcRepository.java
+++ src/main/java/kz/aa/store/items/repository/ItemJdbcRepository.java
@@ -2,16 +2,14 @@ package kz.aa.store.items.repository;
 
 import kz.aa.store.global.model.base.BaseItem;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 
@@ -24,16 +22,18 @@ public class ItemJdbcRepository {
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
-    public Page<BaseItem> findAllItems(Pageable pageable) {
+    public List<BaseItem> findAllItems(Pageable pageable) {
         String query = "";
         SqlParameterSource parameterSource = new MapSqlParameterSource();
-        List<Object> content = namedParameterJdbcTemplate.queryForList(query, parameterSource)
-                .stream()
-                .map()
-                .collect(Collectors.toList());
 
-        return new PageImpl<>(content, pageable, );
+        return namedParameterJdbcTemplate.queryForList(query, parameterSource)
+                .stream()
+                .map(this::map)
+                .collect(Collectors.toList());
     }
 
     // TODO: 22.12.19 map and count add to util
+    private BaseItem map(Map<String, Object> result) {
+        return new BaseItem();
+    }
 }
